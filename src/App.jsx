9b2e30860a83f0cfe4777c816b00f4dab404b781
naru7023 <104113307+naru7023@users.noreply.github.com>
2022-05-18 17:00:28 +0900

    220518-01

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
+import ColorfulMessage from "./components/ColorfulMessage";
 
 const App = () => {
-  const onClickButton = () => alert();
-  const contentStyle = {
-    color: "blue",
-    fontSize: "18px"
+  const [num, setNum] = useState(0);
+  const [faceShowFlag, setFaceShowFlag] = useState(false);
+
+  const onClickCountUp = () => {
+    setNum(num + 1);
   };
+
+  const onClickSwitchShowFlag = () => {
+    setFaceShowFlag(!faceShowFlag);
+  };
+
+  if (num % 3 === 0) {
+    faceShowFlag || setFaceShowFlag(true);
+  } else {
+    faceShowFlag && setFaceShowFlag(false);
+  }
+
   return (
     <>
       <h1 style={{ color: "red" }}>こんにちは！</h1>
-      <p style={contentStyle}>お元気ですか？</p>
-      <button onClick={onClickButton}>ボタン</button>
+      <ColorfulMessage color="blue">お元気ですか？</ColorfulMessage>
+      <ColorfulMessage color="pink">元気です！</ColorfulMessage>
+      <button onClick={onClickCountUp}>カウントアップ</button>
+      <br />
+      <button onClick={onClickSwitchShowFlag}>On/Off</button>
+      <p>{num}</p>
+      {faceShowFlag && <p>＼(^o^)／</p>}
     </>
   );
 };
